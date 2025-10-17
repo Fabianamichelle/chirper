c44ed4a45478cec313e71e4f7212c03eb7be5243
fabiana <fabianamichellee@gmail.com>
2025-10-17 00:15:56 -0600

    Create Full CRUD Functionality

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/' , [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
+
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
